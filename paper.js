9ad11863e5bacec070eef4b8d87d0cce9e88ce5f
satyamshahi73939 <81573078+satyamshahi73939@users.noreply.github.com>
2021-04-28 15:09:07 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -1,9 +1,9 @@
 class Paper {
     constructor(x, y, radius) {
       var options = {
-          'restitution':0.9,
-          'friction':8.5,
-          'density':7.5
+          'restitution':0.15,
+          'friction':200.5,
+          'density':13.5
       }
       this.body = Bodies.circle(x, y, 20,options);
       this.radius = 20;
--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup() {
 
 	//Create the Bodies Here.
     ground = new Ground(width-400,height-10,width,20);
-    paper = new Paper(50, 50, 20);
+    paper = new Paper(30, 600, 20);
     trash_bin1 = new Trash_Bin(700, 630, 150, 10);
     trash_bin2 = new Trash_Bin(630, 595, 10, 80);
     trash_bin3 = new Trash_Bin(770, 595, 10, 80);
@@ -41,7 +41,7 @@ function draw() {
 function keyPressed(){
   if(keyCode === UP_ARROW){
     paper.display();
-    Matter.Body.applyForce(paper.body,paper.body.position,{x:9,y:-9});
+    Matter.Body.applyForce(paper.body,paper.body.position,{x:0.5,y:-33});
 
   }
 }
